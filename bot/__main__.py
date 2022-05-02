2cb277bd75bb9d4615740964650d904f118cf78c
samirrorzone <104243638+samirrorzone@users.noreply.github.com>
2022-05-02 12:53:44 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -43,20 +43,20 @@ def stats(update, context):
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>Bot Uptime:</b> {currentTime}\n\n'\
-            f'<b>Total Disk Space:</b> {total}\n'\
-            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\
-            f'<b>Upload:</b> {sent}\n'\
-            f'<b>Download:</b> {recv}\n\n'\
-            f'<b>CPU:</b> {cpuUsage}%\n'\
-            f'<b>RAM:</b> {mem_p}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
-            f'<b>Physical Cores:</b> {p_core}\n'\
-            f'<b>Total Cores:</b> {t_core}\n\n'\
-            f'<b>SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
-            f'<b>Memory Total:</b> {mem_t}\n'\
-            f'<b>Memory Free:</b> {mem_a}\n'\
-            f'<b>Memory Used:</b> {mem_u}\n'
+    stats = f'<b>➜Bot Uptime:</b> {currentTime}\n\n'\
+            f'<b>➜Total Disk Space:</b> {total}\n\n'\
+            f'<b>➜Used:</b> {used}\n\n<b>Free:</b> {free}\n\n'\
+            f'<b>➜Upload:</b> {sent}\n\n'\
+            f'<b>➜Download:</b> {recv}\n\n'\
+            f'<b>➜CPU:</b> {cpuUsage}%\n\n'\
+            f'<b>➜RAM:</b> {mem_p}%\n\n'\
+            f'<b>➜DISK:</b> {disk}%\n\n'\
+            f'<b>➜Physical Cores:</b> {p_core}\n\n'\
+            f'<b>➜Total Cores:</b> {t_core}\n\n'\
+            f'<b>➜SWAP:</b> {swap_t}\n\n<b>Used:</b> {swap_p}%\n'\
+            f'<b>➜Memory Total:</b> {mem_t}'\
+            f'<b>➜Free:</b> {mem_a}'\
+            f'<b>➜Used:</b> {mem_u}'
     sendMessage(stats, context.bot, update)
 
 
